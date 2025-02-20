85f5d35b69005bf3174a41dfaa4511228918a11e
Luis <iluisflores@yahoo.com>
2025-02-20 00:21:26 -0600

    created variables and event listeners in script.js

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 // Write your code here
-console.log('hello world')
+let lightbulb1 = document.querySelector('#lightbulb1');
+let lightbulb2 = document.querySelector('#lightbulb2');
+let lightbulb3 = document.querySelector('#lightbulb3');
+let subtitle = document.querySelector('.subtitle');
+let count = 0;
+
+lightbulb1.addEventListener('click', function(){
+    count++
+    subtitle.innerHTML = `You've clicked the lights ${count} times`
+    lightbulb1.classList.toggle('active')
+})
+
+lightbulb2.addEventListener('click', function(){
+    count++
+    subtitle.innerHTML = `You've clicked the lights ${count} times`
+    lightbulb2.classList.toggle('active')
+})
+
+lightbulb3.addEventListener('click', function(){
+    count++
+    subtitle.innerHTML = `You've clicked the lights ${count} times`
+    lightbulb3.classList.toggle('active')
+})
